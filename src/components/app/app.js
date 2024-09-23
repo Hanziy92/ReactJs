2a46f49d2c3074ea2aa3f268009b267a0f0f5ada
Your Name <you@example.com>
2024-09-23 17:24:52 +0300

    Props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+   const data = [
+      {name: 'Jonh C.', salary: 8000, increase: false},
+      {name: 'Sam O.', salary: 3000, increase: true},
+      {name: 'Anna K', salary: 5000, increase: false}
+   ];
+
    return (
       <div className="app">
          <AppInfo/>
@@ -15,7 +22,7 @@ function App() {
             <SearchPanel/>
             <AppFilter/>
          </div>
-         <EmployeerList/>
+         <EmployeerList data={data}/>
          <EmployeesAddForm/>
       </div>
    )
--- src/components/employees-list/employeer-list.js
+++ src/components/employees-list/employeer-list.js
@@ -1,12 +1,19 @@
 import EmployeerListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css'
 
-const EmployeerList = () => {
+const EmployeerList = ({data}) => {
+
+
+   const elements = data.map(item => {
+      return (
+         // name={item.name} salary={item.salary}
+         <EmployeerListItem {...item}/>
+      )
+   })
+
    return (
       <ul className="app-list list-group">
-         <EmployeerListItem/>
-         <EmployeerListItem/>
-         <EmployeerListItem/>
+         {elements}
       </ul>
    );
 

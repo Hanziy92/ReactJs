71cd039634b9e194aef852eabfb5518381e1b190
Your Name <you@example.com>
2024-09-23 12:35:18 +0300

    reactComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
+const Header = () => {
+	return (
+		<h2>
+			Hello World!
+		</h2>
+	);
+}
+
+// const Field = () => {
+// 	const holder = 'Enter here';
+// 	const styledField = {
+// 		width: '300px'
+// 	};
+// 	return (
+// 		<input 
+// 			placeholder={holder} 
+// 			type="text" 
+// 			style={styledField}/>
+// 	);
+// }
+
+class Field extends Component {
+	render() {
+		const holder = 'Enter here';
+		const styledField = {
+			width: '300px'
+		};
+
+		return <input 
+					placeholder={holder} 
+					type="text" 
+					style={styledField}/>
+
+	}
+}
+
+function Btn() {
+	// const res = () => {
+	// 	return 'Log In'
+	// }
+	// const p = <p>Log In</p>
+	const text = 'Log In';
+	// const logged = true;
+	const logged = false;
+	return <button>{logged ? 'Enter' : text}</button>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+   	<div className="App">
+			<Header/>
+			<Field/>
+			<Btn/>
+   	</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
-const tex = 'hello world';
-const elm =(
-	<div>
-		<h2 >
-			 Text: {tex}
-		</h2>
-		<input type="text" />
-		<button tabIndex={0}>Click</button>
-	</div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-elm
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 
